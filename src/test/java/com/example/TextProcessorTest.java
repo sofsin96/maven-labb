d434e4b6acdffd36cf07d2e56547e8e9727d3883
Sofia Sinani <sofsin1208@gmail.com>
2020-11-14 11:26:55 +0100

    Edited TextProcessorTest

--- src/test/java/com/example/TextProcessorTest.java
+++ src/test/java/com/example/TextProcessorTest.java
@@ -3,8 +3,6 @@ package com.example;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TextProcessorTest {
@@ -13,21 +11,18 @@ public class TextProcessorTest {
     @RepeatedTest(5)
     void checkToSeeIf_StringToUpperCase_Works() {
         String input = "java";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
         assertNotNull(textProcessor.inputToUpperCase(input));
     }
 
     @Test
     void checkToSeeIf_StringToLowerCase_Works() {
         String input = "";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
         assertTrue(textProcessor.inputToLowerCase(input).isEmpty());
     }
 
     @Test
     void checkToSeeIf_ReverseString_Works() {
         String input = "java";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
         assertEquals("avaj", textProcessor.reverseInput(input));
     }
 }
